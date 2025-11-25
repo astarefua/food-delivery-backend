fb0f5c3c93d61730ec227a99f5106be9106559c6
astarefua <aestherefua@gmail.com>
2025-11-25 14:05:06 +0000

    Core setup: env, cors, logs, error handling, app bootstrap

---
+++ src/config/cors.js
@@ -0,0 +1,8 @@
+import cors from "cors"
+
+export const corsConfig = cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+})
